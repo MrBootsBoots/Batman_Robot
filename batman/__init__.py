2c5d653deffd01798ab32ef26c23ac0ecf54ae82
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-21 15:58:27 +0530

    Update __init__.py

--- batman/__init__.py
+++ batman/__init__.py
@@ -4,10 +4,6 @@ import sys
 
 import telegram.ext as tg
 
-print("batman")
-print("Starting...")
-
-
 # enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -16,9 +12,9 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
-#if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-#    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
-#``````````````    quit(1)
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
+    quit(1)
 
 ENV = bool(os.environ.get('ENV', False))
 
@@ -53,17 +49,18 @@ if ENV:
     CERT_PATH = os.environ.get("CERT_PATH")
 
     DB_URI = os.environ.get('DATABASE_URL')
+    DONATION_LINK = os.environ.get('DONATION_LINK')
     LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
+    NO_LOAD = os.environ.get("NO_LOAD", "").split()
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
-    STRICT_ANTISPAM = bool(os.environ.get('STRICT_ANTISPAM', False))
+    STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
     WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADEAgAAgi3GQL9YQyT_kBpQwI')
+    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    API_WEATHER = os.environ.get('API_OPENWEATHER', None)
+    STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
 
 else:
-    from haruka.config import Development as Config
+    from tg_bot.config import Development as Config
     TOKEN = Config.API_KEY
     try:
         OWNER_ID = int(Config.OWNER_ID)
@@ -98,14 +95,16 @@ else:
     LOAD = Config.LOAD
     NO_LOAD = Config.NO_LOAD
     DEL_CMDS = Config.DEL_CMDS
-    STRICT_ANTISPAM = Config.STRICT_ANTISPAM
+    STRICT_GBAN = Config.STRICT_GBAN
     WORKERS = Config.WORKERS
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
-    API_WEATHER = Config.API_OPENWEATHER
-
+    STRICT_GMUTE = Config.STRICT_GMUTE
+   
 
 SUDO_USERS.add(OWNER_ID)
+
+
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
 
@@ -116,12 +115,10 @@ WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
 
 # Load at end to ensure all prev variables have been set
-from haruka.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, GbanLockHandler
+from batman.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
 
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
 
 if ALLOW_EXCL:
     tg.CommandHandler = CustomCommandHandler
-
-tg.CommandHandler = GbanLockHandler
