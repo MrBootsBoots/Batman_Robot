68ed3801b06c23b3aa24aa9b51de790fd20665a2
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-29 10:29:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from batman.sample_config import Config
 
 
 class Development(Config):
-    OWNER_ID = 254318997  # my telegram ID
+    OWNER_ID = 1180249738  # my telegram ID
     OWNER_USERNAME = "i_am_an_princes"  # my telegram username
     API_KEY = "your bot api key"  # my api key, as provided by the botfather
     DB_URI = 'mysql80-afe9.euw2.cloud.ametnes.com'  # sample db credentials
@@ -11,7 +11,7 @@ class Development(Config):
     SUDO_USERS = [18673980, 83489514]  # List of id's for users which have sudo access to the bot.
     LOAD = []
     NO_LOAD = ['sed']
-    AI_API_KEY = "AI_API_KEY get from coffeehouse.intellivoid.net" #ai api key by intellivoid
+    AI_API_KEY = '4d12b1129a21dcd4d828b128bd28df83253865a8b9c8b0926037db9614720118d88b5082deed4afd444abb26e581cef44ab840e613dcd14aa358a7185895c9b3' #ai api key by intellivoid
     INFOPIC = True
     WHITELIST_USERS = 
     STRICT_GMUTE = True  
