ef7370a56052056b7e6f9a8431a0a94beacc2d67
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-29 10:24:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Development(Config):
     OWNER_ID = 254318997  # my telegram ID
     OWNER_USERNAME = "i_am_an_princes"  # my telegram username
     API_KEY = "your bot api key"  # my api key, as provided by the botfather
-    DB_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
+    DB_URI = 'mysql80-afe9.euw2.cloud.ametnes.com'  # sample db credentials
     MESSAGE_DUMP = '-1004567890' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
     SUDO_USERS = [18673980, 83489514]  # List of id's for users which have sudo access to the bot.
