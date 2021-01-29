9e47e9e59243426474933d54a70f19a14eb940b3
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-29 03:30:11 +0000

    Update chatbot.py

--- batman/modules/chatbot.py
+++ batman/modules/chatbot.py
@@ -159,7 +159,7 @@ dispatcher.add_handler(CHATBOT_HANDLER)
 dispatcher.add_handler(LIST_CB_CHATS_HANDLER)
 
 __mod_name__ = "Chatbot"
-__command_list__ = ["addchat", "rmchat", "listaichats"]
+__command_list__ = ["enable_ai", "disable_ai", "listaichats"]
 __handlers__ = [
     ADD_CHAT_HANDLER, REMOVE_CHAT_HANDLER, CHATBOT_HANDLER,
     LIST_CB_CHATS_HANDLER
