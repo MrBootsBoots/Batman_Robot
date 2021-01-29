1befe2ede56c206856a0b75fc78c4d46e1d1b788
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-29 16:04:52 +0530

    Update disable_sql.py

--- batman/modules/sql/disable_sql.py
+++ batman/modules/sql/disable_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from batman.modules.sql.disable_sql import BASE, SESSION
+from batman.modules.sql import BASE
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
 
