f6d4ac67bf9c59dec6ddca7233bee4cc9466997f
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-29 13:27:48 +0530

    Update __init__.py

--- batman/__init__.py
+++ batman/__init__.py
@@ -60,7 +60,7 @@ if ENV:
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
 
 else:
-    from config.py import Development as Config
+    from batman.sample_config import Development as Config
     TOKEN = Config.API_KEY
     try:
         OWNER_ID = int(Config.OWNER_ID)
